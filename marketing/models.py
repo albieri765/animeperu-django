5a2aa56471e96745e3c2f4e33d6a385a1612f8bc
albieri765 <albieriaqperu@gmail.com>
2025-06-30 11:49:59 -0500

    Permite correo duplicado en ContestEntry

--- marketing/models.py
+++ marketing/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class ContestEntry(models.Model):
-    email     = models.EmailField(unique=True)
+    email      = models.EmailField()    
     nombre    = models.CharField(max_length=100)
     anime_fav = models.CharField(max_length=100)
     creado    = models.DateTimeField(auto_now_add=True)
